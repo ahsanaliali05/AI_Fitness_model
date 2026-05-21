4935a45d380db33e1dd01042a1c31bb978d577b1
ahsanaliali05 <ahsanali.48636@gmail.com>
2026-05-21 22:41:03 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import api from '../api';
+import api from './api';
 import { FiUserPlus, FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 export default function Register() {
